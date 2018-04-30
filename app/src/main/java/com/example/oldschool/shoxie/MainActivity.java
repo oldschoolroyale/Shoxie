61ec968b230479d645c3176ecf33ae9c12a4f79f
Shokhsulton Rakhimjonov <38427828+oldschoolroyale@users.noreply.github.com>
2018-05-01 00:44:01 +0200

    Header added

--- app/src/main/java/com/example/oldschool/shoxie/MainActivity.java
+++ app/src/main/java/com/example/oldschool/shoxie/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.oldschool.shoxie;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int LAYOUT = R.layout.activity_main;
+    private DrawerLayout drawerLayout;
+    private android.support.v7.widget.Toolbar toolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         initToolbar();
+        initNavigationView();
 
 
 
@@ -21,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initToolbar() {
-        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.toolbar);
+         toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
             @Override
@@ -31,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+
+
+
 
     }
 }
