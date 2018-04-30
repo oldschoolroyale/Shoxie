f6ad3c034f7c070d5225c75dd1940efb8d4c997d
Shokhsulton Rakhimjonov <38427828+oldschoolroyale@users.noreply.github.com>
2018-04-30 22:58:26 +0200

    Toolbar adding

--- app/src/main/java/com/example/oldschool/shoxie/MainActivity.java
+++ app/src/main/java/com/example/oldschool/shoxie/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.oldschool.shoxie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+
+
+
+
+    }
+
+    private void initToolbar() {
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+
     }
 }
